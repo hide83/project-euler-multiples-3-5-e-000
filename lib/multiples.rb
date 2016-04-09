db4d68e8b7d005b9ac3dd372854f6b8b3d894af0
hide83 <hiral.desai83@gmail.com>
2016-04-09 05:06:56 +0000

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,22 @@
 # Enter your procedural solution here!
+require 'pry'
+def collect_multiples(limit)
+
+   (1..limit-1).collect do |i|
+     if i % 3 == 0 || i % 5 == 0
+       i
+      end
+   end.compact
+end
+
+    def sum_multiples(limit)
+      array = (1..limit-1).collect do |i|
+     if i % 3 == 0 || i % 5 == 0
+       i
+      end
+   end.compact
+      sum = 0
+      array.collect do |x|
+        sum += x
+      end.last
+  end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,23 @@
 # Enter your object-oriented solution here!
+class Multiples
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def collect_multiples
+
+   (1..@limit-1).collect do |i|
+     if i % 3 == 0 || i % 5 == 0
+       i
+      end
+   end.compact
+end
+   def sum_multiples
+      
+      sum = 0
+      collect_multiples.collect do |x|
+      sum += x
+      end.last
+  end
+end
